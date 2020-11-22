4782c76615a21e6fa9a3042f5f933dc9c193c5fb
Jett Thompson <jett.thompson.59@gmail.com>
2020-11-22 17:29:06 +1100

    removed the main method from the Jcrypt class

--- src/main/java/app/cleancode/Jcrypt.java
+++ src/main/java/app/cleancode/Jcrypt.java
@@ -3,13 +3,6 @@ package app.cleancode;
 import java.security.SecureRandom;
 
 public class Jcrypt {
-public static void main(String[] args) {
-	byte[] key = generateKey(2048);
-	byte[] encryptedBytes = encryptBytes("Hello, World, ".repeat(256).getBytes(), key);
-	System.out.println("Encrypted data: "+new String(encryptedBytes));
-	byte[] decryptedData = encryptBytes(encryptedBytes, key);
-	System.out.println("Decrypted: "+new String(decryptedData));
-}
 public static byte[] generateKey(int length) {
 	SecureRandom rand = new SecureRandom();
 	byte[] key = new byte[length];
